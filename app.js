6c1dcce029d7ff2299220d39d92cdf3ef9b21c1b
kadar naruto <khadarpaashe123@gmail.com>
2025-01-14 10:19:49 +0300

    yesterday and today i was working with mongo db and learning the crud operations also setted mongo in my local also in atlas cloud, additionaly i'm studying mongoose now

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
 const express = require('express');
+
+console.log(process.env);
 
 const app = express();
 const userRouter = require('./routes/userRoutes');
@@ -10,8 +15,9 @@ app.use(express.static(`${__dirname}/public`));
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
 
-module.exports = app;
+const x = 10;
 
+module.exports = app;
 // tichnique one
 // app.delete('/api/v1/tours/:id', deleteTour);
 // app.get('/api/v1/tours', getAllTours);
--- server.js
+++ server.js
@@ -1,4 +1,20 @@
+const mongoose = require('mongoose');
+
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(con => console.log('DB connection successful!', con.connections));
 
 const port = 3000;
 app.listen(port, () => {
